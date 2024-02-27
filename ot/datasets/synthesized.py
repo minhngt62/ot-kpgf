4fad5e6f912de2fc08cebc60648eb62be5dd4cb8
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2024-02-27 23:03:26 +0700

    Exclude random_state

--- ot/datasets/synthesized.py
+++ ot/datasets/synthesized.py
@@ -11,25 +11,24 @@ def gaussMixture_meanRandom_covWishart(
     d_proj: Optional[int] = 5,
     means: Optional[np.ndarray] = None,
     covs: Optional[Union[np.ndarray, List]] = None,
-    random_state: int = 0
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     assert n % k == 0, "Each clusters should have equal number of samples."
 
     # Generate random means for each cluster
     if means is None:
-        means = np.random.randn((k, d), random_state=random_state)
+        means = np.random.randn((k, d))
     
     # Sample covariance matrices from a Wishart distribution
     if covs is None:
         covs = []
         for _ in range(k):
-            cov = scipy.stats.wishart.rvs(df=d, scale=np.eye(d), random_state=random_state)
+            cov = scipy.stats.wishart.rvs(df=d, scale=np.eye(d))
             covs.append(cov)
     
     # Generate data points for each cluster
     X, y = [], []
     for i in range(k):
-        Xi = np.random.multivariate_normal(means[i], covs[i], size=n, random_state=random_state)
+        Xi = np.random.multivariate_normal(means[i], covs[i], size=n)
         Xi = np.concatenate((means[i][None, :], Xi))
         X.append(Xi) # (n, d)
         y.append(i * np.ones(n // k + 1))
@@ -38,5 +37,5 @@ def gaussMixture_meanRandom_covWishart(
     # Randomly project data points to a 5-dimensional subspace
     proj_mat = np.ones((d, d))
     if d_proj is not None:
-        proj_mat = np.random.randn((d, d_proj), random_state=random_state)
+        proj_mat = np.random.randn((d, d_proj))
     return np.dot(X, proj_mat), y, means
