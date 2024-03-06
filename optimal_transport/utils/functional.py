b5af15ba0ad476a2f17fc25f59ff0b0bf329f1c6
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2024-03-06 10:57:36 +0700

    Softmax to softmax matrix

--- optimal_transport/utils/functional.py
+++ optimal_transport/utils/functional.py
@@ -3,5 +3,5 @@ import numpy as np
 
 def softmax(x: np.ndarray) -> np.ndarray:
     y = np.exp(x - np.max(x))
-    f_x = y / np.sum(np.exp(x - np.max(x)))
+    f_x = y / np.sum(np.exp(x - np.max(x)), axis=-1, keepdims=True)
     return f_x
